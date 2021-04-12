bfd5b38c8cb6ef8cad3000b37578319e4b0238c4
mokamokononono <76233144+mokamokononono@users.noreply.github.com>
2021-04-12 19:53:03 +0900

    Userモデルのバリデーション設定
    ・空データの禁止
    ・ニックネームの一意性制約

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nick_name, presence: true
+  validates :kanji_family_name, presence: true
+  validates :kanji_given_name, presence: true
+  validates :katakana_family_name, presence: true
+  validates :katakana_given_name, presence: true
+  validates :date_of_birth, presence: true
+
+  validates :nick_name, uniqueness: true
 end
