7a898e04b3d4d64473ca09f03644e44bba4263f8
mokamokononono <76233144+mokamokononono@users.noreply.github.com>
2021-04-12 20:55:39 +0900

    Userモデル単体テストコードの記述

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :user do
-    
+    nick_name             {Faker::Name.initials(number: 5)}
+    kanji_family_name     {Faker::Name.initials(number: 2)}
+    kanji_given_name      {Faker::Name.initials(number: 2)}
+    katakana_family_name  {Faker::Name.initials(number: 3)}
+    katakana_given_name   {Faker::Name.initials(number: 3)}
+    date_of_birth         {Faker::Date.birthday(min_age: 18, max_age: 65)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,77 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    context '新規登録できるとき' do
+      it 'nick_nameとemail、passwordとpassword_confirmation, kanji_family_nameとkanji_given_name, katakana_family_nameとkatakana_given_name, date_of_birthが存在すれば登録できる' do
+        expect(@user).to be_valid
+      end
+    end
+    context '新規登録できないとき' do
+      it 'nick_nameが空では登録できない' do
+        @user.nick_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Nick name can't be blank"
+      end
+      it 'kanji_family_nameが空では登録できない' do
+        @user.kanji_family_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Kanji family name can't be blank"
+      end
+      it 'kanji_given_nameが空では登録できない' do
+        @user.kanji_given_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Kanji given name can't be blank"
+      end
+      it 'katakana_family_nameが空では登録できない' do
+        @user.katakana_family_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Katakana family name can't be blank"
+      end
+      it 'katakana_given_nameが空では登録できない' do
+        @user.katakana_given_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Katakana given name can't be blank"
+      end
+      it 'emailが空では登録できない' do
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Email can't be blank"
+      end
+      it 'passwordが空では登録できない' do
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Password can't be blank"
+      end
+      it 'passwordが存在してもpassword_confirmationが空では登録できない' do
+        @user.password_confirmation = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Password confirmation doesn't match Password"
+      end
+      it '重複したemailが存在する場合登録できない' do
+        @user.save
+        another_user = FactoryBot.build(:user)
+        another_user.email = @user.email
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include "Email has already been taken"
+      end
+      it '重複したnick_nameが存在する場合登録できない' do
+        @user.save
+        another_user = FactoryBot.build(:user)
+        another_user.nick_name = @user.nick_name
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include "Nick name has already been taken"
+      end
+      it 'passwordが5文字以下では登録できない' do
+        @user.password = '12345'
+        @user.password_confirmation = '12345'
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Password is too short (minimum is 6 characters)"
+      end
+    end
+  end
 end
