3e9a6e047b0da2070bf5ae64261498bd3784ee88
mokamokononono <76233144+mokamokononono@users.noreply.github.com>
2021-04-14 22:09:48 +0900

    商品出品機能のviewファイル実装
    ・newのviewファイル編集
    ・バリデーション
    　・price
    ・課題点
    　・エラーメッセージ表示ができない

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :move_to_signin, except: [:index, :show]
 
   def index
+    # @items = Item.order("created_at DESC") //（メモ）商品表示機能で利用するかも
   end
 
   def new
--- app/models/item.rb
+++ app/models/item.rb
@@ -11,7 +11,7 @@ class Item < ApplicationRecord
       validates :price
       validates :description
       validates :image
-      with_options numericality: { other_than: 1 } do
+      with_options numericality: { other_than: 1, message: "can't be blank" } do
         validates :category_id
         validates :condition_id
         validates :shipping_charge_id
@@ -19,6 +19,8 @@ class Item < ApplicationRecord
         validates :shipping_date_id
       end
   end
+  validates :price, format: { with: /\A[0-9]+\z/, message: "is invalid. Input half-width numbers."}
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"} 
 
   belongs_to :user
   has_one_attached :image
