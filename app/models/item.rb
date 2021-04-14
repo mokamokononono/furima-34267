94e1065a6eca222be3989022f671f9d5a090c631
mokamokononono <76233144+mokamokononono@users.noreply.github.com>
2021-04-14 21:04:30 +0900

    ActiveHashの導入
    ・gem install
    ・ActiveHashのクラス継承
    ・バリデーション
    ・アソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,23 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_charge
+  belongs_to :shipping_source
+  belongs_to :shipping_date
 
   with_options presence: true do
       validates :name
       validates :price
       validates :description
-      validates :category_id
-      validates :condition_id
-      validates :shipping_charge_id
-      validates :shipping_source_id
-      validates :shipping_date_id
       validates :image
+      with_options numericality: { other_than: 1 } do
+        validates :category_id
+        validates :condition_id
+        validates :shipping_charge_id
+        validates :shipping_source_id
+        validates :shipping_date_id
+      end
   end
 
   belongs_to :user
