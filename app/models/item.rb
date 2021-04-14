20ead33f497a345840afb7ff54746edce0691fb7
mokamokononono <76233144+mokamokononono@users.noreply.github.com>
2021-04-14 20:13:20 +0900

    itemsのバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,17 @@
 class Item < ApplicationRecord
+
+  with_options presence: true do
+      validates :name
+      validates :price
+      validates :description
+      validates :category_id
+      validates :condition_id
+      validates :shipping_charge_id
+      validates :shipping_source_id
+      validates :shipping_date_id
+      validates :image
+  end
+
   belongs_to :user
   has_one_attached :image
 end
