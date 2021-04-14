e15db9e607650ee0c50b0fcfb495e1c0b6e6641d
mokamokononono <76233144+mokamokononono@users.noreply.github.com>
2021-04-14 21:25:06 +0900

    商品出品機能のページ遷移実装
    ・ログイン/ログアウトユーザーの区別
    　・controllerでindex,show以外をリダイレクト設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,17 @@
 class ItemsController < ApplicationController
+  before_action :move_to_signin, except: [:index, :show]
+
   def index
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  private
+  def move_to_signin
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
   end
 end
