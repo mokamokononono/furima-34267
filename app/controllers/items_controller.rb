ccb4fc6ac9c6787d1e91c46845c2d61ccc7280c5
mokamokononono <76233144+mokamokononono@users.noreply.github.com>
2021-04-17 18:53:18 +0900

    コントローラの修正
    ・共通した処理のprivateメソッド化
    ・updateアクションのリダイレクト設定追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, except: [:index, :new, :create, :show, :update]
+  before_action :move_to_index, except: [:index, :new, :create, :show]
+
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.order('created_at DESC')
@@ -20,15 +22,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -45,5 +44,9 @@ class ItemsController < ApplicationController
 
   def move_to_index
     redirect_to action: :index unless current_user.id == Item.find(params[:id]).user.id
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
