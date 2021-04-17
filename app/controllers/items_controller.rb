03909b229434716074371c6569277cd65b91cfc0
mokamokononono <76233144+mokamokononono@users.noreply.github.com>
2021-04-17 17:52:10 +0900

    編集ページのリダイレクト設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :new, :create, :show, :update]
 
   def index
     @items = Item.order("created_at DESC")
@@ -40,5 +41,11 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :price, :description, :image, :category_id, :condition_id, :shipping_charge_id,
                                  :shipping_source_id, :shipping_date_id).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless current_user.id == Item.find(params[:id]).user.id
+      redirect_to action: :index
+    end
   end
 end
