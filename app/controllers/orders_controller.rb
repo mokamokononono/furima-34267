af826632b9ddf28dd6004a72c9c2002b4020ff12
mokamokononono <76233144+mokamokononono@users.noreply.github.com>
2021-05-03 23:46:07 +0900

    商品購入画面への遷移設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,6 @@
 class OrdersController < ApplicationController
   def index
-  end
-
-  def new
+    @order_address = OrderAddress.new
   end
 
   def create
--- app/models/order_addresses.rb
+++ app/models/order_addresses.rb
@@ -15,7 +15,7 @@ class OrderAddress
   end
 
   def save
-    order = Order.create(user_id: user_id, item_id:, item_id)
+    order = Order.create(user_id: user_id, item_id: item_id)
     # 住所を保存する
     # donation_idには、変数donationのidと指定する
     Address.create(zip_code: zip_code, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_number: phone_number, order_id: order.id)
