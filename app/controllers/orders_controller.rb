b610a344bb31acd62000868a860f692d51a5c717
mokamokononono <76233144+mokamokononono@users.noreply.github.com>
2021-05-04 20:39:18 +0900

    ソースコードの可読性向上とテストコードの追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,8 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_item, only: [:index, :create]
   before_action :move_to_index_ordered_item
   before_action :move_to_index_myself_item
-
-  before_action :set_item, only: [:index, :create]
 
   def index
     @order_address = OrderAddress.new
@@ -38,13 +37,11 @@ class OrdersController < ApplicationController
   end
 
   def move_to_index_myself_item
-    item = Item.find(params[:item_id])
-    redirect_to root_path if current_user.id == item.user.id
+    redirect_to root_path if current_user.id == @item.user.id
   end
 
   def move_to_index_ordered_item
-    item = Item.find(params[:item_id])
-    redirect_to root_path unless item.order.nil?
+    redirect_to root_path unless @item.order.nil?
   end
 
   def set_item
